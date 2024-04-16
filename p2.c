af2a475a869ce0a5329e5bbdf3a5eb3dc0277fc0
marianogk <marianokpo7@gmail.com>
2024-04-16 16:03:57 -0300

    punto c listo

--- p2.c
+++ p2.c
@@ -4,8 +4,8 @@
 
 int main()
 {
-    int matriz[5][12];
- 
+    int matriz[5][12], suma = 0, promedio;
+
     srand(time(NULL));
 
     for (int i = 0; i < 5; i++)
@@ -17,14 +17,32 @@ int main()
     }
 
     puts("Matriz");
+
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 12; j++)
         {
-            printf("%d ",matriz[i][j]);
+            printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
+
+    // c) sacar promedio //
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            suma = suma + matriz[i][j];
+        }
+
+        promedio = suma / 12;
+
+        printf("\nPromedio del ano %d\n",i+1);
+        printf("%d",promedio);
+    }
+
+    
 
     return 0;
 }
