930a91cd84d4ade40bb11b640ef326fe083e443b
marianogk <marianokpo7@gmail.com>
2024-04-25 16:34:35 -0300

    terminado punto 3

--- p3.c
+++ p3.c
@@ -2,25 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 
+void cargarNombres(char **nombres);
+void mostrarNombres(char **nombres);
+
 int main()
 {
-    char *buff, **nombre;
-    buff = (char *)malloc(100 * sizeof(char));
+    char *buff, **nombres;
+
+    nombres = (char **)malloc(5 * sizeof(char *)); // Asigno memoria a la lista de 5 nombres
+
+    cargarNombres(nombres);
+    mostrarNombres(nombres);
+
+    return 0;
+}
+
+void cargarNombres(char **nombres)
+{
+    char *buff;
+
+    buff = (char *)malloc(100 * sizeof(char)); // Asigno memoria dinamica al buffer para 100 caracteres
 
     for (int i = 0; i < 5; i++)
     {
         puts("Ingrese un nombre: ");
         gets(buff);
 
-        **nombre = (char **)malloc(5 * sizeof(char));
-        strcpy(nombre, buff);
-
-        for (int j = 0; j < strlen(buff) + 1; j++)
-        {
-            *nombre = (char *)malloc((strlen(buff) + 1) * sizeof(char));
-            strcpy(nombre, buff);
-        }
+        nombres[i] = (char *)malloc((strlen(buff) + 1) * sizeof(char)); // Asigno memoria a cada nombre ingresado, con la longitud + 1 para el '/0'
+        strcpy(nombres[i], buff);
     }
+}
 
-    return 0;
+void mostrarNombres(char **nombres)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        printf("\nNombre %d: ", i + 1);
+        puts(nombres[i]);
+    }
 }
